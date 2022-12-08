6061ab0365dd92eb39bdf5d108435ba77fcdf9fe
c0771866 <106362650+c0771866@users.noreply.github.com>
2022-12-08 01:28:34 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "this" {
     ami           = "ami-0a7154091c5c6623e"
     instance_type = "t2.micro"
-    security_groups = ["open-all"]
+    security_groups = ["sg-023fda18436fcac10"]
     key_name      =  "donrabii"
     tags = {
         Name = " tomcat"
