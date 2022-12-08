fa3cde0a0ddf2bd045ace12509234c435e247c41
c0771866 <106362650+c0771866@users.noreply.github.com>
2022-12-08 00:50:40 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
       region = "ca-central-1"
 }
 resource "aws_instance" "app" {
-    ami           = "ami-0b97ca2f2497c83d2"
+    ami           = "ami-0a7154091c5c6623e"
     instance_type = "t2.micro"
     security_groups = ["open-all"]
     key_name      =  "donrabii"
